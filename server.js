dcba07c7e12678cb14d54a3cdfbd233f4712b4a5
mdottavi <maurizio.dottavi.ext@nokia.com>
2025-06-06 12:02:05 +0200

    url params

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 app.get('/', function (req, res) {
   res.send('Hello World')
 });
+
 app.get('/books', function (req, res) {
     let autore=req.query.autore;
     let titolo=req.query.titolo;
@@ -25,11 +26,32 @@ app.get('/books', function (req, res) {
     
   res.send('Ecco la lista dei libri per la ricerca:' + queryfinale);
 });
+
+
 app.get('/booksuuu', function (req, res) {
   res.send('Ecco la lista dei libri')
 });
 app.post('/books', function (req, res) {
   res.send('Ho modificato il tuo libro')
 });
+
+function getLibro2 (req,res){
+   res.send("Qui non passo mai");
+} 
+function getLibro (req,res){
+    if (req.params.idlibro) {
+        res.send ("Nome libro trovato:" + req.params.idlibro);
+    } else {
+        res.status(404);
+        res.send("libro NON trovato");
+    }
+}
+
+app.get('/books/:idlibro',getLibro) ;
+app.get('/books/ciao', function (req, res) {
+  res.send('Quest è Ciao');
+});
+app.get('/books/:idlibro',getLibro2) ;
+
 
 app.listen(3000);
