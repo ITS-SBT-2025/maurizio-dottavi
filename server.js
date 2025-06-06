13d483c13efae350c120af0188721f811c0ffc55
mdottavi <maurizio.dottavi.ext@nokia.com>
2025-06-06 14:33:05 +0200

    read file sync

--- server.js
+++ server.js
@@ -1,78 +1,111 @@
 const express = require('express')
 const app = express();
-const path=require('path');
-const fs=require('fs');
+const path = require('path');
+const fs = require('fs');
 
 
-app.use("/",express.static(path.join(__dirname,"/public"), {"extensions":["html"]}));
+app.use("/", express.static(path.join(__dirname, "/public"), { "extensions": ["html"] }));
 app.use(express.urlencoded());
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+    res.send('Hello World')
 });
 
 
-function cercaLibri  (req, res) {
-    let autore=req.params.autore;
-    let titolo=req.query.titolo;
+function cercaLibri(req, res) {
+    let autore = req.params.autore;
+    let titolo = req.query.titolo;
 
     if (req.params.autore) {
-        autore=req.query.autore;
+        autore = req.query.autore;
     }
-    
-    let parametri_di_ricerca=[];
+
+    let parametri_di_ricerca = [];
 
     let dbquery = "select name,author from books";
-    if (autore){
-        parametri_di_ricerca.push("author like '%"+autore+"%'");
+    if (autore) {
+        parametri_di_ricerca.push("author like '%" + autore + "%'");
     }
-    if (titolo){
-         parametri_di_ricerca.push("title like '%"+titolo+"%'");
+    if (titolo) {
+        parametri_di_ricerca.push("title like '%" + titolo + "%'");
     }
-    
-    let queryfinale=dbquery ;
-    if (parametri_di_ricerca.length >0) {
-        queryfinale+= " WHERE ";
-        queryfinale+= parametri_di_ricerca.join(" AND ");
+
+    let queryfinale = dbquery;
+    if (parametri_di_ricerca.length > 0) {
+        queryfinale += " WHERE ";
+        queryfinale += parametri_di_ricerca.join(" AND ");
     }
-    
-  res.send('Ecco la lista dei libri per la ricerca:' + queryfinale);
+
+    res.send('Ecco la lista dei libri per la ricerca:' + queryfinale);
 }
 
-app.get('/books', cercaLibri);
+
 
 
 app.get('/booksuuu', function (req, res) {
-  res.send('Ecco la lista dei libri')
+    res.send('Ecco la lista dei libri')
 });
 
 
-function getLibro2 (req,res){
-   res.send("Qui non passo mai");
-} 
-function getLibro (req,res){
+function getLibro2(req, res) {
+    res.send("Qui non passo mai");
+}
+function getLibro(req, res) {
     if (req.params.idlibro) {
-        res.send ("Nome libro trovato:" + req.params.idlibro);
+        res.send("Nome libro trovato:" + req.params.idlibro);
     } else {
         res.status(404);
         res.send("libro NON trovato");
     }
 }
-app.get('/books/:autore',cercaLibri) ;
-app.get('/books/:idlibro',getLibro) ;
+app.get('/books/:autore', cercaLibri);
+app.get('/books/:idlibro', getLibro);
 app.get('/books/ciao', function (req, res) {
-  res.send('Quest è Ciao');
+    res.send('Quest è Ciao');
 });
-app.get('/books/:idlibro',getLibro2) ;
+app.get('/books/:idlibro', getLibro2);
 
 
 app.post('/books', function (req, res) {
-  let a=req.body.autore;
-  let t=req.body.titolo;
-  
+    let a = req.body.autore;
+    let t = req.body.titolo;
 
-  res.send('Ho creato il tuo libro: ' + t + " scritto da " + req.body.autore);
+
+    res.send('Ho creato il tuo libro: ' + t + " scritto da " + req.body.autore);
 });
+
+app.get('/books', getBooks);
+
+function getBooks(req, res) {
+    let autore = req.query.autore;
+    let titolo = req.query.titolo;
+    let data = BooksSync(autore,titolo );
+    console.log("=======================");
+    console.log(data);
+    console.log("=======================");
+    res.json(data);
+}
+
+
+
+function BooksSync(a,t) {
+    const data = [];
+    try {
+        let content = fs.readFileSync(path.join(__dirname, 'data/books.dat'), 'utf8');
+        console.log(content);
+        content.split("\n").forEach((line) => {
+            const [title, author] = line.split(";");
+            if (title && author) {
+                console.log(`Nome: ${title}, Autore: ${author}`);
+                data.push({ title, author });
+            }
+        });
+
+    } catch (err) {
+        console.error(err);
+    }
+    return data;
+}
 
 
 app.listen(3000);
